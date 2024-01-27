40efbf11e818e38017cca12c5bab1cdbf2ec6761
Monil Mody <mm01479n@pace.edu>
2024-01-26 19:16:01 -0500

    1/26/2024 - 7:15 : levelup project

---
+++ src/main/java/com/levelup/springjwt/controllers/TestController.java
@@ -0,0 +1,37 @@
+package com.levelup.springjwt.controllers;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+  @GetMapping("/all")
+  public String allAccess() {
+    return "Public Content.";
+  }
+
+  @GetMapping("/user")
+  @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+  public String userAccess(Model model) {
+	model.addAttribute("something", "this is coming from the controller");
+    return "contentpage";
+  }
+
+  @GetMapping("/mod")
+  @PreAuthorize("hasRole('MODERATOR')")
+  public String moderatorAccess() {
+    return "Moderator Board.";
+  }
+
+  @GetMapping("/admin")
+  @PreAuthorize("hasRole('ADMIN')")
+  public String adminAccess() {
+    return "Admin Board.";
+  }
+}
---
+++ src/main/java/com/levelup/springjwt/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.levelup.springjwt.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.levelup.springjwt.models.ERole;
+import com.levelup.springjwt.models.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+  Optional<Role> findByName(ERole name);
+}
